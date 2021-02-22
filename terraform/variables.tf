3a6768d723c838897e6eb3fc81fa3188125f0128
javierwhj <javierwhj@gmail.com>
2021-02-22 13:33:28 +0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,10 +5,10 @@ variable "prefix" {
 
 variable location {
   description = "The Azure location where all resources in this example should be created"
-  default = "southeastasia"
+  default     = "southeastasia"
 }
 
 variable "subscription_id" {
   description = "Azure Subscription ID to be used for billing"
-  default = "d24dfc8f-a319-4d5e-a87f-628be1fc95e2"
+  default     = "d24dfc8f-a319-4d5e-a87f-628be1fc95e2"
 }
